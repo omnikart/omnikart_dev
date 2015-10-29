19510a021462547f7abcd33ea506c4b89aafa45b
unbeatable <mandar@omnikart.com>
2015-10-29 22:42:18 +0530

    Discount

--- catalog/controller/module/featured.php
+++ catalog/controller/module/featured.php
@@ -71,6 +71,8 @@ class ControllerModuleFeatured extends Controller {
 						'name'        => $product_info['name'],
 						'description' => utf8_substr(strip_tags(html_entity_decode($product_info['description'], ENT_QUOTES, 'UTF-8')), 0, $this->config->get('config_product_description_length')) . '..',
 						'price'       => $price,
+						'original_price' => $original_price,
+						'discount' => $discount,
 						'minimum'     => $product_info['minimum'] > 0 ? $product_info['minimum'] : 1,
 						'special'     => $special,
 						'tax'         => $tax,
