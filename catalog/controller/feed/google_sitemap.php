7981bec7028f27efc2797ce7408ae142414e4f4d
unbeatable <mandar@omnikart.com>
2015-11-14 13:51:03 +0530

    SEO GOOGLE SITEMAP

--- catalog/controller/feed/google_sitemap.php
+++ catalog/controller/feed/google_sitemap.php
@@ -1,15 +1,26 @@
 <?php
 class ControllerFeedGoogleSitemap extends Controller {
+	private $j=0;
+	private $i=0;
+	private $outputd;
+	private $output;
+	private $indexing = array();
+	public $timestamp;
 	public function index() {
 		if ($this->config->get('google_sitemap_status')) {
-			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
-			$output .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
-
+			$date = new DateTime();
+			$this->timestamp = date_format($date, 'Y-m-d H:i:s');
+			$i=0;
+			$outputd  = '<?xml version="1.0" encoding="UTF-8"?>';
+			$outputd .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
+			$this->outputd = $outputd;
 			$this->load->model('catalog/product');
 			$this->load->model('tool/image');
+			$directory = "/home/unbeatable/public_html/opencart/sitemaps/";
+			$output = $this->outputd;
 
 			$products = $this->model_catalog_product->getProducts();
-
+			
 			foreach ($products as $product) {
 				if ($product['image']) {
 					$output .= '<url>';
@@ -23,12 +34,38 @@ class ControllerFeedGoogleSitemap extends Controller {
 					$output .= '</image:image>';
 					$output .= '</url>';
 				}
+				$i++;
+				if ($i>200){
+					$output .= '</urlset>';
+					$xmlobj=new SimpleXMLElement($output);
+					$xmlobj->asXML($directory.'products-'.$this->j.'.xml');
+					$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/products-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+					$output = $this->outputd;
+					$this->j++;
+					$i=0;
+				}
 			}
-
+			$output .= '</urlset>';
+			$xmlobj=new SimpleXMLElement($output);
+			$xmlobj->asXML($directory.'products-'.$this->j.'.xml');
+			$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/products-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+			$output = $outputd;
+			$this->j++;
+			$i=0;
+			$this->i=0;
+			
+			$this->output = $outputd;
 			$this->load->model('catalog/category');
-
-			$output .= $this->getCategories(0);
-
+			$this->getCategories(0,'');
+			
+			$this->output .= '</urlset>';
+			$xmlobj=new SimpleXMLElement($this->output);
+			$xmlobj->asXML($directory.'category-'.$this->j.'.xml');
+			$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/category-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+			$this->output = $this->outputd;
+			$this->j++;
+			$this->i=0;
+			
 			$this->load->model('catalog/manufacturer');
 
 			$manufacturers = $this->model_catalog_manufacturer->getManufacturers();
@@ -39,7 +76,16 @@ class ControllerFeedGoogleSitemap extends Controller {
 				$output .= '<changefreq>weekly</changefreq>';
 				$output .= '<priority>0.7</priority>';
 				$output .= '</url>';
-
+				$i++;
+				if ($i>40000){
+					$output .= '</urlset>';
+					$xmlobj=new SimpleXMLElement($output);
+					$xmlobj->asXML($directory.'manf-'.$this->j.'.xml');
+					$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/manf-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+					$output = $this->outputd;
+					$this->j++;
+					$i=0;
+				}
 				$products = $this->model_catalog_product->getProducts(array('filter_manufacturer_id' => $manufacturer['manufacturer_id']));
 
 				foreach ($products as $product) {
@@ -48,9 +94,28 @@ class ControllerFeedGoogleSitemap extends Controller {
 					$output .= '<changefreq>weekly</changefreq>';
 					$output .= '<priority>1.0</priority>';
 					$output .= '</url>';
+
+					$i++;
+					if ($i>40000){
+						$output .= '</urlset>';
+						$xmlobj=new SimpleXMLElement($output);
+						$xmlobj->asXML($directory.'manf-'.$this->j.'.xml');
+						$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/manf-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+						$output = $this->outputd;
+						$this->j++;
+						$i=0;
+					}
+					
 				}
 			}
-
+			$output .= '</urlset>';
+			$xmlobj=new SimpleXMLElement($output);
+			$xmlobj->asXML($directory.'manf-'.$this->j.'.xml');
+			$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/manf-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+			$output = $this->outputd;
+			$this->j++;
+			$i=0;
+			
 			$this->load->model('catalog/information');
 
 			$informations = $this->model_catalog_information->getInformations();
@@ -64,17 +129,37 @@ class ControllerFeedGoogleSitemap extends Controller {
 			}
 
 			$output .= '</urlset>';
+			$xmlobj=new SimpleXMLElement($output);
+			$xmlobj->asXML($directory.'info-'.$this->j.'.xml');
+			$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/info-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+			$output = $this->outputd;
+			$this->j++;
+			$i=0;
+			
+			
+			$output  = '<?xml version="1.0" encoding="UTF-8"?>';
+			$output .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">';
 
-			$this->response->addHeader('Content-Type: application/xml');
-			$this->response->setOutput($output);
+			foreach ($this->indexing as $filelink) {
+					$output .='<sitemap>';
+					$output .='<loc>'.$filelink['loc'].'</loc>';
+					$output .='<lastmod>'.$filelink['lastmod'].'</lastmod>';
+					$output .='</sitemap>';
+			}
+			$output .= '</sitemapindex>';
+			$xmlobj=new SimpleXMLElement($output);
+			$xmlobj->asXML($directory.'sitemap_index'.$this->timestamp.'.xml');
+			
+			//$this->response->addHeader('Content-Type: application/xml');
+			//$this->response->setOutput($output);
 		}
 	}
 
 	protected function getCategories($parent_id, $current_path = '') {
-		$output = '';
-
+		echo $this->i."<br>";
 		$results = $this->model_catalog_category->getCategories($parent_id);
-
+		$directory = "/home/unbeatable/public_html/opencart/sitemaps/";
+		$output = true;
 		foreach ($results as $result) {
 			if (!$current_path) {
 				$new_path = $result['category_id'];
@@ -82,25 +167,43 @@ class ControllerFeedGoogleSitemap extends Controller {
 				$new_path = $current_path . '_' . $result['category_id'];
 			}
 
-			$output .= '<url>';
-			$output .= '<loc>' . $this->url->link('product/category', 'path=' . $new_path) . '</loc>';
-			$output .= '<changefreq>weekly</changefreq>';
-			$output .= '<priority>0.7</priority>';
-			$output .= '</url>';
-
+			$this->output .= '<url>';
+			$this->output .= '<loc>' . $this->url->link('product/category', 'path=' . $new_path) . '</loc>';
+			$this->output .= '<changefreq>weekly</changefreq>';
+			$this->output .= '<priority>0.7</priority>';
+			$this->output .= '</url>';
+			$this->i++;
+			echo $this->i."<br>";
+			if ($this->i>40000){
+					$this->output .= '</urlset>';
+					$xmlobj=new SimpleXMLElement($this->output);
+					$xmlobj->asXML($directory.'category-'.$this->j.'.xml');
+					$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/category-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+					$this->output = $this->outputd;
+					$this->j++;
+					$this->i=0;
+			}
 			$products = $this->model_catalog_product->getProducts(array('filter_category_id' => $result['category_id']));
 
 			foreach ($products as $product) {
-				$output .= '<url>';
-				$output .= '<loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id']) . '</loc>';
-				$output .= '<changefreq>weekly</changefreq>';
-				$output .= '<priority>1.0</priority>';
-				$output .= '</url>';
+				$this->output .= '<url>';
+				$this->output .= '<loc>' . $this->url->link('product/product', 'path=' . $new_path . '&product_id=' . $product['product_id']) . '</loc>';
+				$this->output .= '<changefreq>weekly</changefreq>';
+				$this->output .= '<priority>1.0</priority>';
+				$this->output .= '</url>';
+				$this->i++;
+				echo $this->i."<br>";
+				if ($this->i>40000){
+						$this->output .= '</urlset>';
+						$xmlobj=new SimpleXMLElement($this->output);
+						$xmlobj->asXML($directory.'category-'.$this->j.'.xml');
+						$this->indexing[] = array('loc'=>HTTP_SERVER.'sitemaps/category-'.$this->j.'.xml','lastmod'=>$this->timestamp);
+						$this->output = $this->outputd;
+						$this->j++;
+						$this->i=0;
+				}				
 			}
-
-			$output .= $this->getCategories($result['category_id'], $new_path);
+			$this->getCategories($result['category_id'], $new_path);
 		}
-
-		return $output;
 	}
 }
