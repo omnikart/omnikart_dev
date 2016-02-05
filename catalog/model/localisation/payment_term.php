94aaf078c7ace089744d8a04322f0054fe669cb3
unbeatable <mandar@omnikart.com>
2016-02-06 01:30:53 +0530

    Payment_term added

---
+++ catalog/model/localisation/payment_term.php
@@ -0,0 +1,19 @@
+<?php
+class ModelLocalisationPaymentTerm extends Model {
+	public function addPaymentTerms($data) {
+		$this->db->query ( "INSERT INTO " . DB_PREFIX . "payment_term SET name = '" . $this->db->escape ( $data ['name'] ) . "',sort_order = '" . ( int ) $data ['sort_order'] . "'" );
+		$this->cache->delete ( 'payment_terms' );
+	}
+	public function editPaymentTerms($data) {
+		$this->db->query ( "INSERT INTO " . DB_PREFIX . "payment_term SET name = '" . $this->db->escape ( $data ['name'] ) . "',sort_order = '" . ( int ) $data ['sort_order'] . "'" );
+		$this->cache->delete ( 'payment_terms' );
+	}
+	public function getPaymentTerms($data = array()) {
+		if (! $this->cache->get ( 'payment_terms' )) {
+			$query = $this->db->query ( "SELECT * FROM " . DB_PREFIX . "payment_term WHERE 1" );
+			$this->cache->set ( 'payment_terms', $query->rows );
+			return $query->rows;
+		}
+		return $this->cache->get ( 'payment_terms' );
+	}
+}
